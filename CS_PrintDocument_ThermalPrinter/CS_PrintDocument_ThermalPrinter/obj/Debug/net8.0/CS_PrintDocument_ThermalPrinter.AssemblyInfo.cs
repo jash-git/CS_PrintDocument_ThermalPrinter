93aa608137fa650b11d40ad3a0f567f51105e049
Jash.Liao <jash.liao@gmail.com>
2025-08-15 17:40:59 +0800

    Modified 202508151740

--- CS_PrintDocument_ThermalPrinter/CS_PrintDocument_ThermalPrinter/obj/Debug/net8.0/CS_PrintDocument_ThermalPrinter.AssemblyInfo.cs
+++ CS_PrintDocument_ThermalPrinter/CS_PrintDocument_ThermalPrinter/obj/Debug/net8.0/CS_PrintDocument_ThermalPrinter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CS_PrintDocument_ThermalPrinter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b504a8e612072f1298513edffabd97980e44d9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+687fe9fb94c8547ae97d79916072ebbe801903a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CS_PrintDocument_ThermalPrinter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CS_PrintDocument_ThermalPrinter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
